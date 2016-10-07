bd1f6c303755b2cff80f88e7d13c425b5f9348f0
Ross <Ross@ROSS-ASUS>
2016-10-07 11:10:04 +0100

    K.E.V.I.N V01. Construct a primitive working UI for initial development and testing

---
+++ KEVIN/KEVIN/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace KEVIN
+{
+    partial class frmKEVINMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSong = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSkipBackward = new System.Windows.Forms.Button();
+            this.btnSkipForward = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSong
+            // 
+            this.lblSong.AutoSize = true;
+            this.lblSong.Location = new System.Drawing.Point(12, 9);
+            this.lblSong.Name = "lblSong";
+            this.lblSong.Size = new System.Drawing.Size(35, 13);
+            this.lblSong.TabIndex = 0;
+            this.lblSong.Text = "Song:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "--------------------------------------";
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(12, 37);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(126, 37);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(68, 66);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            // 
+            // btnSkipBackward
+            // 
+            this.btnSkipBackward.Location = new System.Drawing.Point(12, 95);
+            this.btnSkipBackward.Name = "btnSkipBackward";
+            this.btnSkipBackward.Size = new System.Drawing.Size(75, 23);
+            this.btnSkipBackward.TabIndex = 5;
+            this.btnSkipBackward.Text = "<<";
+            this.btnSkipBackward.UseVisualStyleBackColor = true;
+            // 
+            // btnSkipForward
+            // 
+            this.btnSkipForward.Location = new System.Drawing.Point(126, 95);
+            this.btnSkipForward.Name = "btnSkipForward";
+            this.btnSkipForward.Size = new System.Drawing.Size(75, 23);
+            this.btnSkipForward.TabIndex = 6;
+            this.btnSkipForward.Text = ">>";
+            this.btnSkipForward.UseVisualStyleBackColor = true;
+            // 
+            // frmKEVINMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(213, 125);
+            this.Controls.Add(this.btnSkipForward);
+            this.Controls.Add(this.btnSkipBackward);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblSong);
+            this.Name = "frmKEVINMain";
+            this.Text = "KEVIN";
+            this.Load += new System.EventHandler(this.frmKEVINMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSong;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSkipBackward;
+        private System.Windows.Forms.Button btnSkipForward;
+    }
+}
+
---
+++ KEVIN/KEVIN/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KEVIN
+{
+    public partial class frmKEVINMain : Form
+    {
+        public frmKEVINMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmKEVINMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
